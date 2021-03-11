a2458f3f7cfe4d0c66a9cdb3bdc3fa5a6b0fca11
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-10 21:58:34 -0600

    Install dotenv

--- models/busquedas.js
+++ models/busquedas.js
@@ -8,6 +8,14 @@ class Busquedas {
         // Leer BD si existe
     }
 
+    get paramsMapBox(){
+        return {
+            'access_token': 'pk.eyJ1IjoiamVzdXNtaXJhbmRhIiwiYSI6ImNrbTRheTVpbzAycmEycXVzaHdlYjdvNm4ifQ.t-QJVwR48aCvtzdcp34JfA',
+            'limit': 5,
+            'language': 'es'                 
+        }
+    }
+
 
     async ciudad( lugar = '' ){
         //Peticion http
@@ -17,7 +25,14 @@ class Busquedas {
 
         try{
 
-        const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Ja.json?access_token=pk.eyJ1IjoiamVzdXNtaXJhbmRhIiwiYSI6ImNrbTRheTVpbzAycmEycXVzaHdlYjdvNm4ifQ.t-QJVwR48aCvtzdcp34JfA&limit=5&language=es');
+            const intance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+                params: this.paramsMapBox
+            });
+
+            const resp = await intance.get();
+
+      //  const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Ja.json?access_token=pk.eyJ1IjoiamVzdXNtaXJhbmRhIiwiYSI6ImNrbTRheTVpbzAycmEycXVzaHdlYjdvNm4ifQ.t-QJVwR48aCvtzdcp34JfA&limit=5&language=es');
         console.log( resp.data );
 
         return[];
